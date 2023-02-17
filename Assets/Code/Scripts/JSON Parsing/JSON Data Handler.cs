80decc9d5500834286a2cc6d0f032579c4f3765a
Sakuya Izayoi <reyd0905@gmail.com>
2023-02-16 21:58:31 -0500

    Built JSON parser for generic items now

--- Assets/Code/Scripts/JSON Parsing/JSON Data Handler.cs
+++ Assets/Code/Scripts/JSON Parsing/JSON Data Handler.cs
@@ -6,6 +6,7 @@ public static class JSONDataHandler
 {
     public static JSONTypes Types;
     public static JSONCategories Categories;
+    public static JSONItems Items;
 
     public static void LoadData()
     {
@@ -14,5 +15,33 @@ public static class JSONDataHandler
 
         jsonTextFile = Resources.Load<TextAsset>("JSON/Items/Categories");
         Categories = JsonUtility.FromJson<JSONCategories>(jsonTextFile.text);
+
+        ParseItemJSON("JSON/Items/Active Modules/Chainguns");
+        ParseItemJSON("JSON/Items/Active Modules/RocketLaunchers");
+        ParseItemJSON("JSON/Items/Passive Modules/CargoExpanders");
+        ParseItemJSON("JSON/Items/Trade Goods/FreshFood");
+        ParseItemJSON("JSON/Items/Trade Goods/FreshWater");
+        ParseItemJSON("JSON/Items/Trade Goods/LuxuryGoods");
+        
+        Debug.Log(Items.data.Length);
+        
+    }
+
+    public static void ParseItemJSON(string filepath)
+    {
+        var jsonTextFile = Resources.Load<TextAsset>(filepath);
+        JSONItems tempData = JsonUtility.FromJson<JSONItems>(jsonTextFile.text);
+        if (Items != null)
+            Items.data = CombineData(Items.data, tempData.data);
+        else
+            Items = tempData;
+    }
+
+    public static ItemData[] CombineData(ItemData[] arr1, ItemData[] arr2)
+    {
+        ItemData[] tempArray = new ItemData[arr1.Length + arr2.Length];
+        arr1.CopyTo(tempArray, 0);
+        arr2.CopyTo(tempArray, arr1.Length);
+        return tempArray;
     }
 }
